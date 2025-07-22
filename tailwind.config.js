c1a0ecab27b85b688f247339eabfc0dd2ccdf5da
hossam <hhassan522993@gmail.com>
2025-07-22 14:13:37 +0300

    hossa

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./index.html"], // ← هذا هو المهم
   theme: {
-    extend: {},
+    extend: {
+        keyframes: {
+        slowBounce: {
+          '0%, 100%': {
+            transform: 'translateY(0)',
+            animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)',
+          },
+          '50%': {
+            transform: 'translateY(-20px)',
+            animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)',
+          },
+        },
+      },
+      animation: {
+        slowBounce: 'slowBounce 3s infinite', // ← تقدر تخليها 4s لو عايز أبطأ
+      },
+    },
   },
   plugins: [],
 };
